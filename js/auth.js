c6cd3df21e728b49db86bd75d1dd2ac715c785fa
thiago constantino <thiagoconstantino@192.168.1.16>
2023-10-05 22:04:41 -0300

    Logica de usuario autenticado

--- js/auth.js
+++ js/auth.js
@@ -2,33 +2,58 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
-authForm.onsubmit = (event) => {
-  const auth = getAuth();
+const auth = getAuth();
+
+// Função que trata a submissão do formulário de autenticação
+authForm.onsubmit = async (event) => {
+  showItem(loading);
+
   const email = authForm.email.value;
   const password = authForm.password.value;
+  const passwordConfirm = authForm.passwordConfirm.value;
 
   event.preventDefault();
-  if (authForm.authFormSubmit.innerHTML == "Acessar") {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((user) => {
-        console.log("Login realizado com sucesso");
-        console.log(user);
-      })
-      .catch((err) => {
-        console.log("Erro ao realizar o login");
-        console.log(err);
-      });
-  } else {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((user) => {
-        console.log("Cadastro realizado com sucesso");
-        console.log(user);
-      })
-      .catch((err) => {
-        console.log("Erro ao realizar o cadastro");
-        console.log(err);
-      });
+
+  try {
+    if (authForm.authFormSubmit.innerHTML == "Acessar") {
+      await signInWithEmailAndPassword(auth, email, password);
+    } else {
+      if (password !== passwordConfirm) {
+        throw new Error("Senha e Confirmação da senha estão diferentes!");
+      }
+
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log("Cadastro realizado com sucesso");
+      console.log(user);
+    }
+  } catch (error) {
+    console.log("Erro ao realizar operação");
+    console.log(error);
+    alert(error.message);
+    hideItem(loading);
   }
 };
+
+// Função que centraliza e trata a autenticação
+onAuthStateChanged(auth, (user) => {
+  hideItem(loading);
+
+  if(user) {
+    showUserContent(user);
+  } else {
+    showAuth();
+  }
+});
+
+// Função que permite o usuário sair da conta dele
+deslogar = async () => {
+  try {
+    await signOut(auth)
+  } catch(error) {
+    alert(error.message);
+  }
+}
--- js/utils.js
+++ js/utils.js
@@ -1,37 +1,68 @@
 // Definido referências para elementos da página
-var authForm = document.getElementById('authForm');
-var authFormTitle = document.getElementById('authFormTitle');
-var register = document.getElementById('register');
-var access = document.getElementById('access');
-var passwordConfirm = document.getElementById('authFormPassordConfirm');
+var authForm = document.getElementById("authForm");
+var authFormTitle = document.getElementById("authFormTitle");
+var register = document.getElementById("register");
+var access = document.getElementById("access");
+var passwordConfirm = document.getElementById("authFormPassordConfirm");
+var loading = document.getElementById("loading");
+var authContent = document.getElementById("auth");
+var userContent = document.getElementById("userContent");
+var userEmail = document.getElementById("userEmail");
 
 // Alterar o formulário de autenticação para o cadastro de novas contas
 function toggleToRegister() {
-    authForm.authFormSubmit.innerHTML = 'Cadastrar conta';
-    authFormTitle.innerHTML = 'Insira seus dados para se cadastrar';
+  authForm.authFormSubmit.innerHTML = "Cadastrar conta";
+  authFormTitle.innerHTML = "Insira seus dados para se cadastrar";
 
-    hideItem(register);
-    showItem(access);
-    showItem(passwordConfirm);
+  hideItem(register);
+  showItem(access);
+  showItem(passwordConfirm);
 }
 
 // Alterar formulário para o acesso de contas já existentes
 function toggleToAccess() {
-    authForm.authFormSubmit.innerHTML = 'Acessar';
-    authFormTitle.innerHTML = 'Acesse sua conta para continuar';
+  authForm.authFormSubmit.innerHTML = "Acessar";
+  authFormTitle.innerHTML = "Acesse sua conta para continuar";
 
-    hideItem(access);
-    hideItem(passwordConfirm);
-    showItem(register);
+  hideItem(access);
+  hideItem(passwordConfirm);
+  showItem(register);
 }
 
 // Simplifica a exibição de elementos da página
 function showItem(item) {
-    item.style.display = 'flex';
-    item.classList.remove('startHidden');
+  if (item.style.display == "flex") {
+    item.hidden = false;
+  } else {
+    item.style.display = "block";
+    item.classList.remove("startHidden");
+  }
 }
 
 // Simplifica a remoção de elementos da página
 function hideItem(item) {
-    item.style.display = 'none';
+  if (item.style.display == "flex") {
+    item.hidden = true;
+  } else {
+    item.style.display = "none";
+  }
+}
+
+// Variável que recebe a função de deslogar dentro do arquivo firebase.js
+var deslogar;
+
+// Mostrar conteúdo para usuários autenticados
+function showUserContent(user) {
+  userEmail.innerHTML = user.email;
+  hideItem(authContent);
+  showItem(userContent);
+}
+
+// Mostrar conteúdo para usuários não autenticados
+function showAuth() {
+  authForm.email.value = "";
+  authForm.password.value = "";
+  authForm.passwordConfirm.value = "";
+  hideItem(userContent);
+  showItem(authContent);
 }
